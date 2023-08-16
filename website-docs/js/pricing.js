67655698fe6aa4afcfa45e774d07183f16b09ed1
decodeseb <seb9Villiers@gmail.com>
2023-08-16 02:10:52 -0400

    added priing slider in pricing page

---
+++ website-docs/js/pricing.js
@@ -0,0 +1,26 @@
+/* function outputUpdate(num) {
+   document.querySelector('#output').value = num;
+}
+ */
+
+const value = document.querySelector("#output");
+const input = document.querySelector("#priceInput");
+
+value.textContent = input.value;
+console.log(value.textContent);
+
+
+input.addEventListener("input", (event) => {
+    if(event.target.value === "0"){
+        value.textContent = "$205.00"
+    } else if(event.target.value === "25") {
+        value.textContent = "$305.00"
+    } else if(event.target.value === "50") {
+        value.textContent = "$405.00"
+    } else if (event.target.value === "75"){
+        value.textContent = "$505.00"
+    } else if (event.target.value === "100"){
+        value.textContent = "$539.00"
+    }
+    /* value.textContent = event.target.value; */
+});
